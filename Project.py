76c66475572932a585f3f118c42972b1dc7babed
Fatima <fatimabenkadourr@gmail.com>
2023-03-20 16:57:19 +0100

    première versions

--- Project.py
+++ Project.py
@@ -3,42 +3,67 @@ import random
 
 random.seed(1)
 
-
 # fonction sigmoide
 def sigmoide(x):
-    return 1 / (1 + exp(-1 * x))
+    return 1 / (1 + exp(-1*x))
 
+# fonction dérivée de la sigmoide
+def sigmoideDerivative(x):
+    return x * (1 - x)
 
-# class neuron comprenant un identifiant et un poids
-class Neuron:
-    def __init__(self, id, weight):
-        self.id = id
+#class link comprenant un poids et deux neurones
+class Link:
+    def __init__(self, weight):
         self.weight = weight
 
+#class neuron comprenant un identifiant et un poids
+class Neuron:
+    def __init__(self, id):
+        self.id = id
+        self.links = [] # liste des liens
+        self.output = 0 # sortie du neurone
 
-# class layer comprenant une liste de neurones
+    # fonction qui calcule la somme pondérée des entrées
+    def sum(self, previousLayer):
+        sum = 0
+        for i in range(len(previousLayer.getNeurons())):
+            sum += previousLayer.neurons[i].output * self.links[i].weight
+        return sum
+
+    # fonction qui calcule la sortie du neurone
+    def output(self, inputs):
+        self.output = sigmoide(self.sum(inputs))
+
+
+#class layer comprenant une liste de neurones
 class Layer:
     def __init__(self, nbrNeurons):
-        self.neurons = []  # liste des neurones
+        self.neurons = [] # liste des neurones
         for i in range(nbrNeurons):
-            self.neurons.append(Neuron(i, random.random() * 2 - 1))  # création d'un neurone
+            self.neurons.append(Neuron(i)) # création d'un neurone
+
+    # getter pour la liste des neurones
+    def getNeurons(self):
+        return self.neurons
 
 
-# class network comprenant une liste de layers et une erreur en entrée
+#class network comprenant une liste de layers et une erreur en entrée
 class Network:
     def __init__(self, error):
-        self.layers = []  # liste des couches
-        self.error = error  # erreur en entrée pour les résultats
-        self.links = []  # liste des liens entre les couches
-        self.values = []  # liste des valeurs des neurones
+        self.layers = [] # liste des couches
+        self.error = error # erreur en entrée pour les résultats
 
-    # fonction d'ajout d'une couche dans le réseau, prend en paramètre un nombre de neurones
+    #fonction d'ajout d'une couche dans le réseau, prend en paramètre un nombre de neurones
     def addLayer(self, nbrNeurons):
-        layer = Layer(nbrNeurons)  # création d'une couche
+        layer = Layer(nbrNeurons) # création d'une couche
+        self.layers.append(layer) # ajout de la couche dans la liste des couches
 
-    def addLayer(self, layer):
-        self.layers.append(layer)
-
+    # fonction qui crée et initialise les liens entre les couches
+    def createLinks(self):
+        for i in range(len(self.layers)-1):
+            for neuron in self.layers[i].neurons:
+                for neuron2 in self.layers[i+1].neurons:
+                    neuron.links.append(Link((random.random()*2-1)/100))
     def getLayer(self, index):
         return self.layers[index]
 
@@ -48,26 +73,10 @@ class Network:
     def getError(self):
         return self.error
 
-    def getLink(self, index):
-        return self.links[index]
-
-    def getLinks(self):
-        return self.links
-
-    def getValue(self, index):
-        return self.values[index]
-
-    def getValues(self):
-        return self.values
-
     def getNbrLayers(self):
         return len(self.layers)
 
-    def getNbrLinks(self):
-        return len(self.links)
 
-    def getNbrValues(self):
-        return len(self.values)
 
-    def getLastLayer(self):
-        return self.layers[-1]
+if __name__ == '__main__':
+    print("test")
